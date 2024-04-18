5420b962f64c654dd0ced0abc3f5548de82a9345
asiaaaaaaaaaaaaa <166690530+asiaaaaaaaaaaaaa@users.noreply.github.com>
2024-04-18 11:20:32 +0200

    wip: refactoring to folders

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from models.data import users
-from utils.crud import show_users
-1
+from utils.crud import show_users,add_new_user,search_users,remove_users
 
 if __name__ == '__main__':
-    print("Witaj użutkowniku")
+    print("Witaj użytkowniku")
     while True:
 
         print("Wybierz opcję menu")
+        print("0. Zakończ program")
         print("1. Wyświetl co u znajomych")
+        print("2. Dodaj użytkownika")
+        print("3. Usuń użytkownika")
         menu_option: str = input("Dokonaj wyboru:")
         if menu_option == "0":
             print("Program kończy pracę")
             break
         if menu_option == "1":
             show_users(users)
+        if menu_option == "2":
+            add_new_user(users)
--- utils/crud.py
+++ utils/crud.py
@@ -2,3 +2,21 @@ def show_users(user_list: list[dict]) -> None:
     for user in user_list:
         print(f"Twój znajomy {user['name']} opublikował: {user['posts']}")
 
+def add_new_user(users:list): -> None:
+    new_name: str = input("Enter your name: ")
+    new_surname: str = input("Enter your surname: ")
+    new_post: int = int(input("Enter your post: "))
+    new_username: dict = {"name": new_name, "surname": new_surname, "post": new_post}
+    print(new_user)
+    users.append(new_user)
+
+
+def search_users(users) -> None:
+
+
+kogo_szukasz = input("Kogo szukasz")
+for user in users:
+    if user in users:
+        if user['name'] == kogo_szukasz:
+            print(user)
+
