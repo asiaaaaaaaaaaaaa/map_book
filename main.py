135c28f3d6c45fd6b27600a1d3aa5164cc9042b6
asiaaaaaaaaaaaaa <166690530+asiaaaaaaaaaaaaa@users.noreply.github.com>
2024-05-15 21:12:08 +0200

    wip: refactoring to folders

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
-from models.data import users
-from utils.crud import show_users,add_new_user,search_users,remove_users
+from models. data import users
+from utils.crud import show_users, add_new_user, search_user, remove_user
+
+
+
+
 
 if __name__ == '__main__':
-    print("Witaj użytkowniku")
+    print("Witaj uzytkowniku")
     while True:
-
-        print("Wybierz opcję menu")
-        print("0. Zakończ program")
-        print("1. Wyświetl co u znajomych")
-        print("2. Dodaj użytkownika")
-        print("3. Usuń użytkownika")
+        print("Menu")
+        print("0. Zakoncz program")
+        print("1. Wyswietl co u zanjomych")
+        print("2. Dodaj uzytkownika")
+        print("3. Znajdz uzytkownika")
+        print("4. Usun uzytownika")
         menu_option: str = input("Dokonaj wyboru:")
         if menu_option == "0":
-            print("Program kończy pracę")
+            print("Program konczy prace")
             break
         if menu_option == "1":
             show_users(users)
         if menu_option == "2":
             add_new_user(users)
+        if menu_option == "3":
+            search_user(users)
+        if menu_option == "4":
+            remove_user(users)
